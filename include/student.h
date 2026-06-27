3222941756b366d1102ecee3ea3d81dc9853db6a
Mohit Dayanand Navare <navaremohit21@gmail.com>
2026-06-27 11:59:10 +0530

    Update student.h

--- include/student.h
+++ include/student.h
@@ -1,1 +1,26 @@
+#ifndef STUDENT_H
+#define STUDENT_H
 
+#define DATA_FILE "data/students.csv"
+
+#define MAX_NAME 50
+#define MAX_GRADE 10
+#define MAX_LINE 200
+
+typedef struct
+{
+    int id;
+    char name[MAX_NAME];
+    int age;
+    char grade[MAX_GRADE];
+} Student;
+
+/* Student Operations */
+
+void addStudent();
+void displayStudents();
+void searchStudent();
+void updateStudent();
+void deleteStudent();
+
+#endif
