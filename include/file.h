b657c64bdaf4d494e5de6f795b5b271e1bc2928d
Mohit Dayanand Navare <navaremohit21@gmail.com>
2026-06-27 11:58:07 +0530

    Update file.h

--- include/file.h
+++ include/file.h
@@ -1,1 +1,16 @@
+#ifndef FILE_H
+#define FILE_H
 
+#include <stdio.h>
+#include "student.h"
+
+// Reads one student record from the file
+int readStudent(FILE *fp, Student *s);
+
+// Writes one student record to the file
+void writeStudent(FILE *fp, Student s);
+
+// Checks whether a student ID already exists
+int studentExists(int id);
+
+#endif
