7973b08d1a12b3ab3f0805d3833efe1036284073
Mohit Dayanand Navare <navaremohit21@gmail.com>
2026-06-27 12:01:26 +0530

    Update utility.c

--- src/utility.c
+++ src/utility.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include "utility.h"
 
+void clearInputBuffer()
+{
+    int ch;
+
+    while ((ch = getchar()) != '\n' && ch != EOF)
+        ;
+}
+
+void pressEnter()
+{
+    printf("\nPress Enter to continue...");
+    getchar();
+}
+
+void printLine(int length)
+{
+    for (int i = 0; i < length; i++)
+        printf("=");
+
+    printf("\n");
+}
